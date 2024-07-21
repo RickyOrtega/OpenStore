6f1d9e508c8bf3efc0fe52fbb24855a12eb7590b
rrovindabank <ortegavitalricky@gmail.com>
2024-07-21 07:28:15 -0500

    Módulo de Eureka-server

---
+++ config-server/src/test/java/com/bigo/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigo.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
